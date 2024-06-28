1aedf7f8a68fa7a5a7ab80acd7d808ee0527e0b9
admrshid <adm.rshid@gmail.com>
2024-06-28 08:38:36 +0100

    fix

--- run/model_train_synthetic.bat
+++ run/model_train_synthetic.bat
@@ -15,26 +15,5 @@ rem change current dir to main repository
 echo Changing directory...
 cd C:\Users\user\Documents\R(2+1)d SAR building change detector
 
-rem set path to which SAR data is saved to
-echo Setting data path...
-set "path=D:\Swindon_geotiffs_2\video_dataset"
-
-rem set decision to form segmented processing
-echo Setting segmentation...
-set "segment=No"
-
-rem set expected class names
-echo Setting class names...
-set "classes=change,no_change"
-
-rem set desired frames
-echo Setting frame parameters...
-set "num_frames=33"
-set "frame_step=11"
-
-rem list out metrics to test
-echo Setting metrics...
-set "metrics=VV,VH,VH_COHERENCE,VV_COHERENCE"
-
 echo Running Python script...
-"%python_executable%" -m model_synthetic !path! !classes! !segment! !num_frames! !frame_step! !metrics!
+"%python_executable%" -m model_synthetic
